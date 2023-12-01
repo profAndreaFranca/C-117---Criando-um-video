2cf6b30b3691a48e39d0f69f36aaefba36458a04
Profª Andréa França <dejandreafranca@gmail.com>
2023-11-30 21:32:54 -0300

    ajustes e testes

--- createVideo.py
+++ createVideo.py
@@ -1,36 +1,26 @@
 import cv2
 import os
 
-<<<<<<< HEAD
-path = "Images"
+path = "./Images"
 images = []
 
 for file in os.listdir(path):
     name, ext = os.path.splitext(file)
-    if ext in ["jpg", "jpeg", "png", "gif", "bmp", "webp"]:
+    if ext in [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".webp"]:
         filename = path + "/" + file
         images.append(filename)
+
 count = len(images)
+# print(count)
 
-=======
-path = "Images"
-images = []
-
-for file in os.listdir(path):
-    name, ext = os.path.splitext(file)
-    if ext in ["jpg", "jpeg", "png", "gif", "bmp", "webp"]:
-        filename = path + "/" + file
-        images.append(filename)
-count = len(images)
-
-frame = cv2.imread(image[0])
+frame = cv2.imread(images[0])
 height,width,channels = frame.shape
 size = (width,height)
 
 output = cv2.VideoWriter("sum.mp4",cv2.VideoWriter_fourcc(*'DIVX'),5,size)
 
 for i in range(count - 1, 0, -1):
-    frame = cv2.imread(image[i])
+    frame = cv2.imread(images[i])
     output.write(frame)
     
 output.release()
