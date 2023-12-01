89a9fb7eaf607eae0d7aa4eb8001b17fed1d45b1
Betaschweigert <107437913+Betaschweigert@users.noreply.github.com>
2023-11-30 21:18:47 -0300

    output

--- createVideo.py
+++ createVideo.py
@@ -10,3 +10,9 @@ for file in os.listdir(path):
         filename = path + "/" + file
         images.append(filename)
 count = len(images)
+
+frame = cv2.imread(image[0])
+height,width,channels = frame.shape
+size = (width,height)
+
+output = cv2.VideoWriter("sum.mp4",cv2.VideoWriter_fourcc(*'DIVX'),5,size)
